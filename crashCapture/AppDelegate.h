e5bc84038739985f058d273171f75c964af97ced
lcjingdi <lcjingdi@gmail.com>
2017-08-16 11:53:15 +0800

    Initial Commit

---
+++ crashCapture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  crashCapture
+//
+//  Created by jingdi on 2017/8/16.
+//  Copyright © 2017年 lcjingdi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ crashCapture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  crashCapture
+//
+//  Created by jingdi on 2017/8/16.
+//  Copyright © 2017年 lcjingdi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
